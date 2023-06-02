d80aadab04bf918276eb04d6253b53ebf6e43435
JKjiwon <jwkim.oa@gmail.com>
2023-06-02 14:57:32 +0900

    feat : 회원목록 및 개인정보 수정 보안 강화

--- src/main/java/next/web/ListUserServlet.java
+++ src/main/java/next/web/ListUserServlet.java
@@ -8,17 +8,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import core.db.DataBase;
+import next.model.User;
 
 @WebServlet("/user/list")
 public class ListUserServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private static final String USER_SESSION_KEY = "user";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = getUser(req.getSession());
+        if (user == null) {
+            resp.sendRedirect("/");
+            return;
+        }
         req.setAttribute("users", DataBase.findAll());
         RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
         rd.forward(req, resp);
+    }
+
+    private User getUser(HttpSession session) {
+        Object value = session.getAttribute(USER_SESSION_KEY);
+        if (value != null) {
+            return (User) value;
+        }
+        return null;
     }
 }
--- src/main/java/next/web/UpdateUserServlet.java
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -9,22 +9,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/update")
 public class UpdateUserServlet extends HttpServlet {
 
+    private static final String USER_SESSION_KEY = "user";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User sessionUser = getUser(req.getSession());
         User user = DataBase.findUserById(req.getParameter("userId"));
+        if (sessionUser == null || !sessionUser.getUserId().equals(user.getUserId())) {
+            resp.sendRedirect("/");
+            return;
+        }
+
         req.setAttribute("user", user);
         RequestDispatcher rd = req.getRequestDispatcher("/user/update_form.jsp");
         rd.forward(req, resp);
-
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User sessionUser = getUser(req.getSession());
+        if (sessionUser == null || !sessionUser.getUserId().equals(req.getParameter("userId"))) {
+            resp.sendRedirect("/");
+            return;
+        }
+
         User user = new User(
                 req.getParameter("userId"),
                 req.getParameter("password"),
@@ -32,5 +46,13 @@ public class UpdateUserServlet extends HttpServlet {
                 req.getParameter("email"));
         DataBase.addUser(user);
         resp.sendRedirect("/user/list");
+    }
+
+    private User getUser(HttpSession session) {
+        Object value = session.getAttribute(USER_SESSION_KEY);
+        if (value != null) {
+            return (User) value;
+        }
+        return null;
     }
 }
