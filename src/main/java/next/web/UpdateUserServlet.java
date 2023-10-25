dad6ad529a87dc17bf1c9b0247848f4361379997
JKjiwon <jwkim.oa@gmail.com>
2023-10-25 15:46:05 +0900

    feat: 6장_요구사항3 구현 - 회원 목록 및 개인정보 수정 보안 강화

--- src/main/java/next/web/UpdateUserServlet.java
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/update")
@@ -20,7 +21,21 @@ public class UpdateUserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+
+        if (value == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+
+        User sessionUser = (User) value;
         String userId = req.getParameter("userId");
+
+        if (!sessionUser.getUserId().equals(userId)) {
+            throw new IllegalStateException("잘못된 접근 입니다.");
+        }
+
         User user = DataBase.findUserById(userId);
         req.setAttribute("user", user);
         RequestDispatcher rd = req.getRequestDispatcher("/user/update_form.jsp");
@@ -29,17 +44,25 @@ public class UpdateUserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+
+        if (value == null) {
+            resp.sendRedirect("/user/login.jsp");
+            return;
+        }
+
+        User sessionUser = (User) value;
         String userId = req.getParameter("userId");
 
-        String updatedPassword = req.getParameter("password");
-        String updatedEmail = req.getParameter("email");
-        String updatedName = req.getParameter("name");
-
-        log.debug("Update User[{}] password={}, name={}, email={}", userId, updatedPassword, updatedName, updatedEmail);
+        if (!sessionUser.getUserId().equals(userId)) {
+            throw new IllegalStateException("잘못된 접근 입니다.");
+        }
 
         User user = DataBase.findUserById(userId);
-        user.update(updatedPassword, updatedName, updatedEmail);
+        user.update(req.getParameter("password"), req.getParameter("name"), req.getParameter("email"));
         DataBase.updateUser(user);
+        log.debug("Update User[{}] password={}, name={}, email={}", userId, user.getPassword(), user.getName(), user.getEmail());
 
         resp.sendRedirect("/user/list");
     }
