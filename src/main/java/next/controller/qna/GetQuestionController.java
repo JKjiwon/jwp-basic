57df8e9091abb521455174f1dd93ea9095aae5d2
JKjiwon <jwkim.oa@gmail.com>
2023-11-05 12:17:13 +0900

    feat: 답변 조회 추가

--- src/main/java/next/controller/qna/GetQuestionController.java
+++ src/main/java/next/controller/qna/GetQuestionController.java
@@ -1,15 +1,22 @@
 package next.controller.qna;
 
 import core.mvc.Controller;
+import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 import next.dao.UserDao;
+import next.model.Answer;
 import next.model.Question;
 import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class GetQuestionController implements Controller {
+
+    private static Logger log = LoggerFactory.getLogger(GetQuestionController.class);
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
@@ -20,8 +27,12 @@ public class GetQuestionController implements Controller {
         String writer = question.getWriter();
         User user = new UserDao().findByUserId(writer);
 
+        List<Answer> answers = new AnswerDao().findAllByQuestionId(questionId);
+        log.info("questionId={}, answer size={}", questionId, answers.size());
+
         req.setAttribute("user", user);
         req.setAttribute("question", question);
+        req.setAttribute("answers", answers);
 
         return "/qna/show.jsp";
     }
