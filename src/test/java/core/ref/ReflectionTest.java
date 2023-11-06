f1eefd115e7985914898b6ce4abd5553cfbc7a12
JKjiwon <jwkim.oa@gmail.com>
2023-11-06 20:12:41 +0900

    test : showClass

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,11 +1,16 @@
 package core.ref;
 
+import next.model.Question;
+import next.model.User;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import next.model.Question;
-import next.model.User;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -13,6 +18,23 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
+        String fields = Arrays.stream(clazz.getDeclaredFields()).map(Field::getName).collect(Collectors.joining(", "));
+        logger.debug("fields={}", fields);
+
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            String name = constructor.getName();
+            String parameterType = Arrays.stream(constructor.getParameterTypes()).map(Class::getName).collect(Collectors.joining(", "));
+            String signature = name + "(" + parameterType + ")";
+            logger.debug("constructor={}", signature);
+        }
+
+        for (Method method : clazz.getDeclaredMethods()) {
+            String name = method.getName();
+            String parameterType = Arrays.stream(method.getParameterTypes()).map(Class::getName).collect(Collectors.joining(", "));
+            String signature = name + "(" + parameterType + ")";
+            logger.debug("method={}", signature);
+        }
+
         logger.debug(clazz.getName());
     }
     
