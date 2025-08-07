775ce358c66f365ce04dfddef068d7d725c16d41
kimjiwon <jwkim.oa@gmail.com>
2025-08-07 16:05:48 +0900

    refactor: sql 변수로 분리

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -12,24 +12,28 @@ public class UserDao {
     private final JdbcTemplate<User> jdbcTemplate = new JdbcTemplate<>();
 
     public void insert(User user) {
-        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) {
-        jdbcTemplate.update("UPDATE USERS SET password=?, name=?, email=? WHERE userId=?", user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+        String sql = "UPDATE USERS SET password=?, name=?, email=? WHERE userId=?";
+        jdbcTemplate.update(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
     }
 
 
     public List<User> findAll() {
+        String sql = "SELECT userId, password, name, email FROM USERS";
         RowMapper<User> rm = rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
                 rs.getString("email"));
-        return jdbcTemplate.query("SELECT userId, password, name, email FROM USERS", null, rm);
+        return jdbcTemplate.query(sql, null, rm);
     }
 
     public User findByUserId(String userId) {
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
         PreparedStatementSetter pstmts = pstmt -> pstmt.setString(1, userId);
         RowMapper<User> rm = rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
                 rs.getString("email"));
-        return jdbcTemplate.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userid=?", pstmts, rm);
+        return jdbcTemplate.queryForObject(sql, pstmts, rm);
     }
 }
